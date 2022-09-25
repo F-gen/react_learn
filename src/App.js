65f4a9a41bf44e537ab93c5981d0a4dd0b3fac7c
F-gen <1101853378@qq.com>
2022-09-25 22:43:40 +0800

    form_control

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 
+// 表单-受控组件
+import React from "react";
+class Counter extends React.Component {
+  state = {
+    message: 'this is message'
+  }
+  inputChange = (e) => {
+    console.log('inputChange');
+    // 修改state中的message
+    this.setState({
+      message: e.target.value
+    })
+  }
+  render() {
+    return (
+      // 绑定input的value值 
+      // 绑定input的onChange事件
+      <input type='text' value={this.state.message} onChange={this.inputChange} ></input>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Counter></Counter>
     </div>
   );
 }
