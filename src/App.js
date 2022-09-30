81880ee0fea3d8be08c5ccb68d350bd625f017f6
F-gen <1101853378@qq.com>
2022-10-01 01:21:57 +0800

    component_transfData

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import React from "react";
 //函数式的Son
 function Son(props) {
   // props 对象  存父组件传递的数据
+  // 解构  const { list } = props    或者 在组件接收处解构  function Son({list,userInfo,getMes,child}) 
+
   return (
     <>
-
+      <button onClick={props.getMes}>触发父组件传入函数</button>
       <div>函数：子组件 ，{props.list} {props.list.map(item => <p key={item}>{item}</p>)}</div>
+      {props.children}
     </>
   )
 }
@@ -35,12 +38,20 @@ class App extends React.Component {
       age: 18
     }
   }
-
+  getMes = () => {
+    console.log(1);
+  }
   render() {
     return (
       <div>
         {/*子组件上绑定数据  属性名自定义  */}
-        <Son msg={this.state.message} list={this.state.list} userInfo={this.state.userInfo}  ></Son>
+        <Son
+          msg={this.state.message}
+          list={this.state.list}
+          userInfo={this.state.userInfo}
+          getMes={this.getMes}
+          children={<div>我是传来的Jsx</div>}
+        ></Son>
         <SonC msg={this.state.message} />
       </div>
     )
